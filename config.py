90be4ba3002ea4c2d02af7e55e15db42a38079d4
ClarriseCabansag <132436016+ClarriseCabansag@users.noreply.github.com>
2024-10-27 18:41:32 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    SECRET_KEY = '0123'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:0802@localhost:5432/POS'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
