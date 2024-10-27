d58e9c92297e421d41a43f4435f8bfd98d678b34
ClarriseCabansag <132436016+ClarriseCabansag@users.noreply.github.com>
2024-10-27 18:39:45 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "0123")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgre_17_user:TOmuUalV9SI8K0uwzrornfb9WRNCWZon@dpg-cscfr356l47c73e0k7ag-a/postgre_17")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
