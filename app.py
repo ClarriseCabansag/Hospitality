e133024d94db185fc059b4f6eba76a781c75cc13
ClarriseCabansag <132436016+ClarriseCabansag@users.noreply.github.com>
2024-10-30 14:50:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1630@localhost:5432/POS'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgre_17_user:TOmuUalV9SI8K0uwzrornfb9WRNCWZon@dpg-cscfr356l47c73e0k7ag-a/postgre_17'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
