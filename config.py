a313d10cec6b623503211b80d6d2164a037c8ef0
ClarriseCabansag <132436016+ClarriseCabansag@users.noreply.github.com>
2024-12-10 10:29:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = '0123'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgre_17_user:TOmuUalV9SI8K0uwzrornfb9WRNCWZon@dpg-cscfr356l47c73e0k7ag-a/postgre_17'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgre_17_h0hq_user:d542cVHrrsO8tqaSoyHPclBNmfBkos4B@dpg-ctbqegd2ng1s73bp2i80-a/postgre_17_h0hq'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
