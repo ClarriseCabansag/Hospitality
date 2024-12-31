ef17b9ee6d1b863db6b00eaa1b6222022958af58
ClarriseCabansag <132436016+ClarriseCabansag@users.noreply.github.com>
2025-01-01 02:52:49 +0800

    Add files via upload

--- models/Till.py
+++ models/Till.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from services.database import db
 
 class OpenTill(db.Model):
@@ -7,3 +6,13 @@ class OpenTill(db.Model):
     amount = db.Column(db.Float)
     time = db.Column(db.String)  # For storing AM/PM formatted time
     date = db.Column(db.String)  # For storing the date in YYYY-MM-DD format
+    cashier_id = db.Column(db.Integer, db.ForeignKey('cashiers.id'))  # Foreign Key to Cashier
+    cashier_username = db.Column(db.String(15))  # Store the username
+    cashier = db.relationship('Cashier', backref=db.backref('tills', lazy=True))  # Establish relationship
+
+    def __init__(self, amount, time, date, cashier_id, cashier_username):
+        self.amount = amount
+        self.time = time
+        self.date = date
+        self.cashier_id = cashier_id
+        self.cashier_username = cashier_username  # Save the username
