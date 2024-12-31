dbaf6d75255123ad38b9e582aa1a11134dd125c6
ClarriseCabansag <132436016+ClarriseCabansag@users.noreply.github.com>
2025-01-01 04:21:51 +0800

    Update Till.py

--- models/Till.py
+++ models/Till.py
@@ -7,8 +7,8 @@ class OpenTill(db.Model):
     time = db.Column(db.String)  # For storing AM/PM formatted time
     date = db.Column(db.String)  # For storing the date in YYYY-MM-DD format
     cashier_id = db.Column(db.Integer, db.ForeignKey('cashiers.id'))  # Foreign Key to Cashier
-  
-
+    cashier_username = db.Column(db.String(15))  # Store the username
+    cashier = db.relationship('Cashier', backref=db.backref('tills', lazy=True))  # Establish relationship
 
     def __init__(self, amount, time, date, cashier_id, cashier_username):
         self.amount = amount
